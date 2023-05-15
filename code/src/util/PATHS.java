ed98908b22df00f4444c2a7c7ad1c1be8a314210
Xiaxia Wang <xxwang@smail.nju.edu.cn>
2023-05-15 18:52:49 +0100

    Update PATHS.java

--- code/src/util/PATHS.java
+++ code/src/util/PATHS.java
@@ -5,7 +5,6 @@ public class PATHS {
     /**
      * Note: Before running the codes, you should change all the paths to your local settings.
      *
-     * @author: Xiaxia Wang
      */
     public static final String FileBase = "D:/Work/File/";
     public static final String HashPath = "D:/Work/Dataset-Hash/";
